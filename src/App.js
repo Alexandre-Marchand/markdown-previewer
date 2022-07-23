bb6554390e0ae84a11afa06afb05897336a57cef
Alexandre-Marchand <marchand.nma@gmail.com>
2022-07-23 15:32:22 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import { marked } from 'marked';
 import './App.css';
 
 function App() {
+
+  const [ texte, setTexte ] = useState('# `Prévisualisateur - Markdown`\n### Ce projet a été réalisé avec React ;)\n\n### Cas d\'utilisation\n* *italique*\n* **gras**\n* `monospace`\n* ~~barré~~\n* # h1\n* ## h2\n* ### h3\n* #### etc\n[Alex](https://github.com/Alexandre-Marchand)');
+
+  const changeTextHandler = (event) => {
+    setTexte(event.target.value);
+    console.log(marked(texte));
+  };
+
+  const mardownToHmtl = () => {
+    return {__html: marked(texte)}
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='elements'>
+
+        <div className="element">
+          <textarea
+            rows="30" 
+            value={texte}
+            onChange={(e) => changeTextHandler(e)}>
+            </textarea>
+        </div>
+          
+        <div className="element">
+          <div dangerouslySetInnerHTML={mardownToHmtl()}></div>
+        </div>
+          
+          
+      </div>
     </div>
   );
 }
